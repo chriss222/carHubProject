94aa860f689cf4bd8af37bf562479c1b5ec04179
Cristian Mitu <cristian.mitu@skywindgroup.com>
2023-08-04 12:00:41 +0300

    work on car showcase section

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,18 @@ import Hero from './Hero';
 import Button from './Button';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import CustomFilter from './CustomFilter';
+import Searchbar from './Searchbar';
+import SearchBrand from './SearchBrand';
+import CarCard from './CarCard';
 
 export {
     Hero,
     Button,
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    Searchbar,
+    SearchBrand,
+    CarCard
 }
